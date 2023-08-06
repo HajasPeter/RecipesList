d05aa4223a74ba05a32d6c281b22267d34a5fcd5
peto <peto.hajas@gmail.com>
2023-08-07 00:14:54 +0200

    Added 'Add to shopping list' functionality

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
@@ -25,7 +27,14 @@ export class RecipeService {
             ])
       ];
 
+      constructor(private shoppingListService: ShoppingListService) {}
+
       getRecipes() {
         return this.recipes.slice();
       }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+      }
+
 }
