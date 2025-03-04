b8c11f1b877e3a9387dba08a36d0aebea9fae7fe
PeterHajas <peto.hajas@gmail.com>
2025-03-04 16:04:44 +0100

    Add token in interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map, take, tap } from "rxjs/operators";
+import { exhaustMap, map, take, tap } from "rxjs/operators";
 import { AuthService } from "../auth/auth.service";
 
 
@@ -28,21 +28,16 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.authService.user.pipe(take(1)).subscribe(user => {
-            
-        } )
-        return this.http.get<Recipe[]>(
-            'https://recipes-list-4ddc3-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
-        )
-        .pipe(
-            map(recipes => {
-            return recipes.map(recipe => {
-                return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
-            })
-            }),
-            tap(recipes => {
-                this.recipeService.setRecipes(recipes);
-            })
-        )
+            return this.http.get<Recipe[]>(
+                'https://recipes-list-4ddc3-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+            ).pipe(
+                map(recipes => {
+                return recipes.map(recipe => {
+                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+                    })
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })) 
     }
 }
